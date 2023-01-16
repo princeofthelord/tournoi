ec4908664f6aab9070230a2a1849a78b58987194
Pindjouf <109728085+pindjouf@users.noreply.github.com>
2023-01-17 00:49:51 +0100

    fichier de base
    on essaye inshallah

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Login</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  <form action="login.php" method="post">
+    <label for="email">Email:</label>
+    <input type="email" id="email" name="email"><br><br>
+    <label for="password">Password:</label>
+    <input type="password" id="password" name="password"><br><br>
+    <input type="submit" value="Login">
+  </form>
+  <br>
+  <p>Don't have an account? <a href="signup.html">Sign Up</a></p>
+</body>
+</html>
+<?php
+
+// echo "FUUUUUUUUUUUUUUUUUCK";
+
+?>
